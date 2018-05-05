cdc8b2ac281603dd565de0ef53e650cc96d96034
Ante <ante.bralic@gmail.com>
2018-05-05 17:36:38 -0400

    Basic character setup with simple locomotion
    - Included plugin folder.
    
    Signed-off-by: Ante <ante.bralic@gmail.com>

---
+++ Assets/[Contents]/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TOJam2018
+{
+    public class PlayerCharacterController : MonoBehaviour 
+    {
+        
+    }
+}
